aa13e2991507b038134ac982887ae54016317259
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-05-10 15:15:43 +0700

    memasukan kode mhs mahasiswa

--- 051_Pointer.cpp
+++ 051_Pointer.cpp
@@ -5,10 +5,26 @@ class mahasiswa {
 public:
 	int nim;
 	void showNim(); //deklarasi method
-};
+}; 
+
 void mahasiswa::showNim() { //implementasi method di luar class
 	cout << "No Induk = " << nim << endl;
 }
 
+int main()
+{
+	mahasiswa mhs{ 1 };   //object mhs
+	mhs.showNim();       //Member Acces Operator
+
+	mahasiswa& ref = mhs;    //pointer Refrence refMhs
+	ref.nim = 2;             //Member Acces Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;  //Pointer Dereference pMhs
+	pMhs->nim = 3;           //Arrow Operator
+	mhs.showNim();
+	system("Pause");
+	return 0;
+}
 
 
